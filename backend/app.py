3162f33be5f50a6b01a480901ff15c5f8174bec1
stone_lord <75131862+serkarim@users.noreply.github.com>
2026-03-11 21:10:34 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -491,7 +491,7 @@ async def get_roles_api():
         raise HTTPException(status_code=500, detail=str(e))
 
 
-if __name__ == "__main__":
-    import uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
